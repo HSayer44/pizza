52ed5dc328c6356b44e38148f157a7ec485d0bd7
Al Sayer <alsayerhaitham@gmail.com>
2024-04-15 10:33:40 +0200

    authentication bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pizza/app.dart';
+import 'package:pizza/simple_bloc_observer.dart';
+import 'package:user_repository/user_repository.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(Placeholder());
+  Bloc.observer = SimpleBlocObserver();
+  runApp(MyApp(FirebaseUserRepo()));
 }
